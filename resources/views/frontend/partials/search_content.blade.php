b8b53309ee369c1356d638215675dd32c2c06ccf
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-01-20 18:02:21 +0500

    Update search_content.blade.php

--- resources/views/frontend/partials/search_content.blade.php
+++ resources/views/frontend/partials/search_content.blade.php
@@ -35,7 +35,7 @@
                                 <img class="w-100  rounded" style="object-fit: contain; aspect-ratio: 1 / 1;" src="{{ uploaded_asset($product->thumbnail_img) }}">
                             </div>
                             <div class="flex-grow-1 overflow--hidden minw-0">
-                                <div class="product-name text-truncate fs-14 mb-5px">
+                                <div class="product-name text-truncate fs-18 mb-5px">
                                     {{  $product->getTranslation('name')  }}
                                 </div>
                                 <div class="d-flex flex-column">
