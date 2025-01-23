80ab462f0c5467658c0dec291edb628c8fe0cfef
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-01-23 11:49:10 +0500

    Update nav.blade.php

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -977,7 +977,7 @@ $(document).ready(function(){
                     <div class="d-flex flex-row justify-content-start align-items-center">
                         <button class="px-1 text-primary g-remove-from-car btn-light bg-white border-0 btn-lg" data-id="${item.id}"><i class="fa fa-trash"></i></button>
                         <div class="position-relative mx-2" style="width: 50px; height: 50px;" >
-                            <img src="${item.image}" alt="${item.name}" class="rounded-2 ">
+                            <img src="${item.image}" alt="${item.name}" class="rounded-2 w-100 h-100 " style="object-fit: contain;">
                             <span class="cart-item-count">${item.quantity}</span>
                         </div>
                         
