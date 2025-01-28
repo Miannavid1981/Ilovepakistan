2bb5db2dae422f08455a520c78d13a9c96be4be0
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-01-28 18:01:30 +0500

    Update newest_products_section.blade.php

--- resources/views/frontend/megamart/partials/newest_products_section.blade.php
+++ resources/views/frontend/megamart/partials/newest_products_section.blade.php
@@ -43,6 +43,12 @@
             <div class="row gx-3">
     @foreach ($newest_products as $key => $new_product)
     <div class="col-6 col-md-4 col-lg custom-col position-relative has-transition hov-animate-outline">
+        @php
+            $seller_map = \App\Models\ProductSellerMap::where('product_id', $product->id)->where('seller_id', $new_product->user_id)->where('source_seller_id',$new_product->user_id )->first();
+            $encrypted_skin = $seller_map->encrypted_hash ?? '';
+            $product_url = url('/product/' . $product->slug . '/' . $encrypted_skin);
+            $product->product_custom_url = $product_url;
+        @endphp
         @include('frontend.'.get_setting('homepage_select').'.partials.product_box_1',['product' => $new_product])
     </div>
     @endforeach
