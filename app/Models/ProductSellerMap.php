36aeab699880cf0fefe247cba21eb6b27b9831f9
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-01-28 16:39:32 +0500

    Update ProductSellerMap.php

--- app/Models/ProductSellerMap.php
+++ app/Models/ProductSellerMap.php
@@ -10,9 +10,9 @@ use App\Models\Product;
 
 class ProductSellerMap extends Model
 {
-    protected $fillable = ['product_id', 'seller_id', 'imported', 'source_seller_id'];
+    protected $fillable = ['product_id', 'seller_id', 'imported', 'source_seller_id', 'sku', 'encrypted_value', 'encrypted_hash', 'original_skin'];
     protected $table = 'product_seller_map';
-    
+
     public function product()
     {
         return $this->belongsTo(Product::class);
