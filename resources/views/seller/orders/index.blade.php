3d8ba65a0216746efe5e201c202d66ed43153b50
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-02-08 20:09:30 +0500

    order test

--- resources/views/seller/orders/index.blade.php
+++ resources/views/seller/orders/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('panel_content')
 
+
     <div class="card">
         <form class="" action="" id="sort_orders" method="GET">
             <div class="card-header row gutters-5">
@@ -89,6 +90,14 @@
                                 @php
                                     $order = \App\Models\Order::find($order_id->id);
                                 @endphp
+
+                                
+@php
+    
+dd($order);
+
+@endphp
+
                                 @if ($order != null)
                                     <tr>
                                         <td>
