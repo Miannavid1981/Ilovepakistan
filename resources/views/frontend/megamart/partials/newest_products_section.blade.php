ce8cfeee4d0709059a66aa43f89b7d029bf5ca52
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-02-10 13:21:14 +0500

    Update newest_products_section.blade.php

--- resources/views/frontend/megamart/partials/newest_products_section.blade.php
+++ resources/views/frontend/megamart/partials/newest_products_section.blade.php
@@ -27,11 +27,11 @@
     <section >
         <div class="px-3">
             <!-- Top Section -->
-            <div class="d-flex mb-2 mb-md-3 mt-2 mt-md-3  align-items-baseline justify-content-between">
+            <div class="d-flex mb-2 mb-md-3 mt-2 align-items-baseline justify-content-between">
                 <!-- Title -->
-                <h3 class="fs-16 fs-md-20 fw-700 mb-2 mb-sm-0">
+                {{-- <h3 class="fs-16 fs-md-20 fw-700 mb-2 mb-sm-0">
                     <span class="">{{ translate('New Products') }}</span>
-                </h3>
+                </h3> --}}
                 <!-- Links -->
                 <!-- <div class="d-flex">
                     <a type="button" class="arrow-prev slide-arrow link-disable text-secondary mr-2" onclick="clickToSlide('slick-prev','section_newest')"><i class="las la-angle-left fs-20 fw-600"></i></a>
