cc56a1cd7e5124c817bc4ea2179a2185955c410c
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-02-15 16:50:03 +0500

    Update addToCart.blade.php

--- resources/views/frontend/partials/cart/addToCart.blade.php
+++ resources/views/frontend/partials/cart/addToCart.blade.php
@@ -4,12 +4,12 @@
             <div class="bg-white py-3">
                 <div class="row">
                     <!-- Product Image Gallery -->
-                    <div class="col-xl-4 col-lg-6 mb-4">
+                    <div class="col-xl-4 col-lg-5 mb-4">
                         @include('frontend.product_details.image_gallery')
                     </div>
 
                     <!-- Product Details -->
-                    <div class="col-xl-8 col-lg-6">
+                    <div class="col-xl-8 col-lg-7">
                         @include('frontend.product_details.details')
                     </div>
                 </div>
