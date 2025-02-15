9651a7ffe890590c18d118b7a78cebfa9d493268
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-02-15 15:42:17 +0500

    Update image_gallery.blade.php

--- resources/views/frontend/product_details/image_gallery.blade.php
+++ resources/views/frontend/product_details/image_gallery.blade.php
@@ -183,14 +183,14 @@ $photos = [];
 </style>
 
 <div class="row g-1">
-    <div class="col-2 col-sm-3">
+    <div class="col-2 ">
         <div class="slider slider-nav">
             @foreach ($photos as $key => $photo)
                 <img src="{{ uploaded_asset($photo) }}" class=" h-100" {{ $loop->first ? 'selected' : '' }}" onclick="changeImage('{{ uploaded_asset($photo) }}', this)">
             @endforeach
         </div>
     </div>
-    <div class="col-10 col-sm-9">
+    <div class="col-10 ">
         <div class="slider slider-for">
             @foreach ($photos as $key => $photo)
                 <img src="{{ uploaded_asset($photo) }}" class=" h-100" {{ $loop->first ? 'selected' : '' }}" onclick="changeImage('{{ uploaded_asset($photo) }}', this)">
