a90e24190ae27fc40a84da447fa7868d5047839a
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-02-25 16:28:04 +0500

    Update app.blade.php

--- resources/views/seller/layouts/app.blade.php
+++ resources/views/seller/layouts/app.blade.php
@@ -95,7 +95,7 @@
 
 	<script src="{{ static_asset('assets/js/vendors.js') }}" ></script>
 	{{-- <script src="{{ static_asset('assets/js/aiz-core.js') }}" ></script> --}}
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> --}}
     @yield('script')
 
     <script type="text/javascript">
