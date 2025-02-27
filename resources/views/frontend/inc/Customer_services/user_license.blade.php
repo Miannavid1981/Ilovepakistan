93450d29b2da093ebdea0f47009740289cc6ea48
gM <Romailanwer7@gmail.com>
2025-02-27 19:50:27 +0500

    footers/navblade

---
+++ resources/views/frontend/inc/Customer_services/user_license.blade.php
@@ -0,0 +1,131 @@
+@extends('frontend.layouts.app')
+
+<style>
+    .user_license{
+        margin-top: 100px;
+        margin-bottom: 100px;
+    }
+</style>
+@section('content')
+    <div class="container">
+        <div class="user_license">
+            <h1 style="font-size: 24px; font-weight: bold; text-align: center;">END USER LICENSE AGREEMENT</h1>
+            <p style="font-size: 16px; font-weight: bold;">Last updated __________</p>
+            
+            <p style="font-size: 16px;">__________ is licensed to You (End-User) by __________, located at __________, __________, __________ __________, __________ ("Licensor"), for use only under the terms of this License Agreement.</p>
+            
+            
+            <ul style="font-size: 16px;">
+                <li>The parties of this License Agreement acknowledge that the Services are not a Party to this License Agreement and are not bound by any provisions or obligations with regard to the Licensed Application, such as warranty, liability, maintenance, and support thereof. __________, not the Services, is solely responsible for the Licensed Application and the content thereof.</li>
+                
+                <li>This License Agreement may not provide for usage rules for the Licensed Application that are in conflict with the latest ("Usage Rules"). __________ acknowledges that it had the opportunity to review the Usage Rules and this License Agreement is not conflicting with them.</li>
+                
+                <li>__________ when purchased or downloaded through the Services, is licensed to You for use only under the terms of this License Agreement. The Licensor reserves all rights not expressly granted to You. __________ is to be used on devices that operate with __________.</li>
+                
+                <li>By downloading the Licensed Application from __________, and any update thereto (as permitted by this License Agreement), You indicate that You agree to be bound by all of the terms and conditions of this License Agreement, and that You accept this License Agreement. Referred to in this License Agreement as "Services."</li>
+            </ul>
+            <h2 style="font-size: 20px; font-weight: bold;">TABLE OF CONTENTS</h2>
+            <ul style="font-size: 16px;">
+                <li>1. THE APPLICATION</li>
+                <li>2. SCOPE OF LICENSE</li>
+                <li>3. TECHNICAL REQUIREMENTS</li>
+                <li>4. NO MAINTENANCE AND SUPPORT</li>
+                <li>5. USER-GENERATED CONTRIBUTIONS</li>
+                <li>6. CONTRIBUTION LICENSE</li>
+                <li>7. LIABILITY</li>
+                <li>8. WARRANTY</li>
+                <li>9. PRODUCT CLAIMS</li>
+                <li>10. LEGAL COMPLIANCE</li>
+                <li>11. CONTACT INFORMATION</li>
+                <li>12. TERMINATION</li>
+                <li>13. THIRD-PARTY TERMS OF AGREEMENTS AND BENEFICIARY</li>
+                <li>14. INTELLECTUAL PROPERTY RIGHTS</li>
+                <li>15. APPLICABLE LAW</li>
+                <li>16. MISCELLANEOUS</li>
+            </ul>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">1. THE APPLICATION</h2>
+            <p style="font-size: 16px;">__________ ("Licensed Application") is a piece of software created to __________ — and customized for mobile devices ("Devices"). It is used to __________.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">2. SCOPE OF LICENSE</h2>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">3. TECHNICAL REQUIREMENTS</h2>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">4. NO MAINTENANCE OR SUPPORT</h2>
+            <p style="font-size: 16px;"><strong>4.1</strong> __________ is not obligated, expressed or implied, to provide any maintenance, technical or other support for the Licensed Application.</p>
+            <p style="font-size: 16px;"><strong>4.2</strong> __________ and the End-User acknowledge that the Services have no obligation whatsoever to furnish any maintenance and support services with respect to the Licensed Application.</p>
+            <h2 style="font-size: 20px; font-weight: bold;">5. USER-GENERATED CONTRIBUTIONS</h2>
+            <p style="font-size: 16px;">The Licensed Application does not offer users to submit or post content. We may provide you with the opportunity to create, submit, post, display, transmit, perform, publish, distribute, or broadcast content and materials to us or in the Licensed Application, including but not limited to text, writings, video, audio, photographs, graphics, comments, suggestions, or personal information or other material (collectively, "Contributions"). Contributions may be viewable by other users of the Licensed Application and through third-party websites or applications. As such, any Contributions you transmit may be treated in accordance with the Licensed Application Privacy Policy. When you create or make available any Contributions, you thereby represent and warrant that:</p>
+            <ul style="font-size: 16px;">
+                <li>Your Contributions do not and will not infringe the proprietary rights, including but not limited to the copyright, patent, trademark, trade secret, or moral rights of any third party.</li>
+                <li>You are the creator and owner of or have the necessary licenses, rights, consents, releases, and permissions to use and to authorize us, the Licensed Application, and other users of the Licensed Application to use your Contributions in any manner contemplated by the Licensed Application and this License Agreement.</li>
+                <li>You have the written consent, release, and/or permission of each identifiable individual person in your Contributions to use their name or likeness to enable inclusion and use of your Contributions in any manner contemplated by the Licensed Application and this License Agreement.</li>
+                <li>Your Contributions are not false, inaccurate, or misleading.</li>
+                <li>Your Contributions are not unsolicited or unauthorized advertising, promotional materials, pyramid schemes, chain letters, spam, mass mailings, or other forms of solicitation.</li>
+                <li>Your Contributions are not obscene, lewd, lascivious, filthy, violent, harassing, libelous, slanderous, or otherwise objectionable (as determined by us).</li>
+                <li>Your Contributions do not ridicule, mock, disparage, intimidate, or abuse anyone.</li>
+                <li>Your Contributions are not used to harass or threaten (in the legal sense of those terms) any other person or to promote violence against a specific person or class of people.</li>
+                <li>Your Contributions do not violate any applicable law, regulation, or rule.</li>
+                <li>Your Contributions do not violate the privacy or publicity rights of any third party.</li>
+                <li>Your Contributions do not violate any applicable law concerning child pornography or otherwise intended to protect the health or well-being of minors.</li>
+                <li>Your Contributions do not include any offensive comments related to race, national origin, gender, sexual preference, or physical handicap.</li>
+                <li>Your Contributions do not otherwise violate, or link to material that violates, any provision of this License Agreement, or any applicable law or regulation.</li>
+            </ul>
+            
+            <p style="font-size: 16px;">Any use of the Licensed Application in violation of the foregoing violates this License Agreement and may result in, among other things, termination or suspension of your rights to use the Licensed Application.</p>
+            <p style="font-size: 16px;">Any use of the Licensed Application in violation of the foregoing violates this License Agreement and may result in, among other things, termination or suspension of your rights to use the Licensed Application.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">6. CONTRIBUTION LICENSE</h2>
+            <p style="font-size: 16px;">You agree that we may access, store, process, and use any information and personal data that you provide following the terms of the Privacy Policy and your choices (including settings).</p>
+            <p style="font-size: 16px;">By submitting suggestions of other feedback regarding the Licensed Application, you agree that we can use and share such feedback for any purpose without compensation to you.</p>
+            <p style="font-size: 16px;">We do not assert any ownership over your Contributions. You retain full ownership of all of your Contributions and any intellectual property rights or other proprietary rights associated with your Contributions. We are not liable for any statements or representations in your Contributions provided by you in any area in the Licensed Application. You are solely responsible for your Contributions to the Licensed Application and you expressly agree to exonerate us from any and all responsibility and to refrain from any legal action against us regarding your Contributions.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">7. LIABILITY</h2>
+            
+            <h2 style="font-size: 20px; font-weight: bold;">8. WARRANTY</h2>
+            <p style="font-size: 16px;"><strong>8.1</strong> Licensor warrants that the Licensed Application is free of spyware, trojan horses, viruses, or any other malware at the time of Your download. Licensor warrants that the Licensed Application works as described in the user documentation.</p>
+            <p style="font-size: 16px;"><strong>8.2</strong> No warranty is provided for the Licensed Application that is not executable on the device, that has been unauthorizedly modified, handled inappropriately or culpably, combined or installed with inappropriate hardware or software, used with inappropriate accessories, regardless if by Yourself or by third parties, or if there are any other reasons outside of __________'s sphere of influence that affect the executability of the Licensed Application.</p>
+            <p style="font-size: 16px;"><strong>8.3</strong> You are required to inspect the Licensed Application immediately after installing it and notify __________ about issues discovered without delay by email provided in Contact Information. The defect report will be taken into consideration and further investigated if it has been emailed within a period of __________ days after discovery.</p>
+            <p style="font-size: 16px;"><strong>8.4</strong> If we confirm that the Licensed Application is defective, __________ reserves a choice to remedy the situation either by means of solving the defect or substitute delivery.</p>
+            <p style="font-size: 16px;"><strong>8.5</strong> In the event of any failure of the Licensed Application to conform to any applicable warranty, You may notify the Services Store Operator, and Your Licensed Application purchase price will be refunded to You. To the maximum extent permitted by applicable law, the Services Store Operator will have no other warranty obligation whatsoever with respect to the Licensed Application, and any other losses, claims, damages, liabilities, expenses, and costs attributable to any negligence to adhere to any warranty.</p>
+            <p style="font-size: 16px;"><strong>8.6</strong> If the user is an entrepreneur, any claim based on faults expires after a statutory period of limitation amounting to twelve (12) months after the Licensed Application was made available to the user. The statutory periods of limitation given by law apply for users who are consumers.</p>
+             
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">9. PRODUCT CLAIMS</h2>
+            <p style="font-size: 16px; color: #444;">__________ and the End-User acknowledge that __________, and not the Services, is responsible for addressing any claims of the End-User or any third party relating to the Licensed Application or the End-User’s possession and/or use of that Licensed Application, including, but not limited to:</p>
+            <ul style="font-size: 16px; color: #444;">
+                <li>(i) product liability claims;</li>
+                <li>(ii) any claim that the Licensed Application fails to conform to any applicable legal or regulatory requirement; and</li>
+                <li>(iii) claims arising under consumer protection, privacy, or similar legislation.</li>
+            </ul>
+            
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">10. LEGAL COMPLIANCE</h2>
+            <p style="font-size: 16px; color: #444;">You represent and warrant that You are not located in a country that is subject to a US Government embargo, or that has been designated by the US Government as a "terrorist supporting" country; and that You are not listed on any US Government list of prohibited or restricted parties.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">11. CONTACT INFORMATION</h2>
+            <p style="font-size: 16px; color: #444;">For general inquiries, complaints, questions or claims concerning the Licensed Application, please contact:</p>
+            <p style="font-size: 16px; color: #444;">__________<br>
+            __________<br>
+            __________, __________ __________<br>
+            __________<br>
+            __________</p>
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">12. TERMINATION</h2>
+            <p style="font-size: 16px; color: #444;">The license is valid until terminated by __________ or by You. Your rights under this license will terminate automatically and without notice from __________ if You fail to adhere to any term(s) of this license. Upon License termination, You shall stop all use of the Licensed Application, and destroy all copies, full or partial, of the Licensed Application.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">13. THIRD-PARTY TERMS OF AGREEMENTS AND BENEFICIARY</h2>
+            <p style="font-size: 16px; color: #444;">__________ represents and warrants that __________ will comply with applicable third-party terms of agreement when using Licensed Application.</p>
+            <p style="font-size: 16px; color: #444;">In Accordance with Section 9 of the "Instructions for Minimum Terms of Developer's End-User License Agreement," subsidiaries shall be third-party beneficiaries of this End User License Agreement and — upon Your acceptance of the terms and conditions of this License Agreement, will have the right (and will be deemed to have accepted the right) to enforce this End User License Agreement against You as a third-party beneficiary thereof.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">14. INTELLECTUAL PROPERTY RIGHTS</h2>
+            <p style="font-size: 16px; color: #444;">__________ and the End-User acknowledge that, in the event of any third-party claim that the Licensed Application or the End-User's possession and use of that Licensed Application infringes on the third party's intellectual property rights, __________, and not the Services, will be solely responsible for the investigation, defense, settlement, and discharge of any such intellectual property infringement claims.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">15. APPLICABLE LAW</h2>
+            <p style="font-size: 16px; color: #444;">This License Agreement is governed by the laws of __________ excluding its conflicts of law rules.</p>
+            
+            <h2 style="font-size: 20px; font-weight: bold; color: #222;">16. MISCELLANEOUS</h2>
+            <p style="font-size: 16px; color: #444;">16.1 If any of the terms of this agreement should be or become invalid, the validity of the remaining provisions shall not be affected. Invalid terms will be replaced by valid ones formulated in a way that will achieve the primary purpose.</p>
+            <p style="font-size: 16px; color: #444;">16.2 Collateral agreements, changes, and amendments are only valid if laid down in writing. The preceding clause can only be waived in writing.</p>
+        
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/frontend/inc/customer_footer.blade.php
@@ -0,0 +1,126 @@
+
+<footer class="footer text-dark pt-5 pb-3" style="background-color: #E8E8E8; line-height: 40px;">
+    <div class="container">
+        <div class="row">
+            <!-- About Section -->
+            <div class="col-lg-2 col-md-6 mb-4 text-center text-md-start">
+                @php
+
+                $header_logo =  get_setting('header_logo');
+                $logo_url = uploaded_asset(get_setting('header_logo'));
+                $my_account_url =  route('profile');
+                if( Auth::user() ) {
+                    $my_account_url = Auth::user()->user_type == "staff" ? '/admin/profile/' : route('profile');
+                }
+                
+                
+            @endphp
+                <a href="{{ url('/') }}">
+                    @if ($header_logo != null)
+
+                        <img src="{{ $logo_url }}" alt="{{ env('APP_NAME') }}" class="img-fluid">
+
+                    @else
+
+                        <img src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}"
+
+                            class="mw-100 h-67px h-md-40px" height="67px">
+
+                    @endif
+                </a>
+                <ul class="list-unstyled">
+                    <li class="fs-17"><strong>Mail:</strong> hi.avitex@gmail.com</li>
+                    <li class="fs-17"><strong>Phone:</strong> 1-333-345-6868</li>
+                    <li class="fs-17"><strong>Address:</strong> 549 Oak St. Crystal Lake, IL 60014</li>
+                </ul>
+            </div>
+            <!-- Information Section -->
+            <div class="col-12 col-lg-2 col-md-6 mb-4 text-center text-md-start">
+                <h5 class="fs-20 fw-bold">Information</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Contact Us</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Career</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">My Account</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Order & Returns</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">FAQs</a></li>
+                </ul>
+            </div>
+            <!-- Quick Shop Section -->
+            <div class="col-12 col-lg-2 col-md-6 mb-4 text-center text-md-start">
+                <h5 class="fs-20 fw-bold fw-bold">Quick Shop</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Women</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Men</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Clothes</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Accessories</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Blog</a></li>
+                </ul>
+            </div>
+            <!-- Customer Services Section -->
+            <div class="col-12 col-lg-2 col-md-6 mb-4 text-center text-md-start">
+                <h5 class="fs-20 fw-bold">Customer Services</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Orders FAQs</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Shipping</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Privacy Policy</a></li>
+                    <li><a href="#" class="text-dark fs-17 text-decoration-none">Return & Refund</a></li>
+                </ul>
+            </div>
+            <!-- Seller Zone Section -->
+            <div class="col-12 col-lg-2 col-md-6 mb-4 text-center text-md-start">
+                <h5 class="fs-20 fw-bold">Business Partner Zone</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-1">
+                        <p class="fs-17 text-dark text-secondary mb-0">
+                          
+                            <a href="{{ route('shops.create') }}" class="fs-17 text-dark">  Be our Partner</a>
+                        </p>
+                    </li>
+                   
+                    <li class="mb-1">
+                        <a class="fs-17 text-dark animate-underline-dark" href="{{ url('/seller/login') }}">Partner Login</a>
+                    </li>
+                   
+                    <li class="mb-1">
+                        <p class="fs-17 text-dark text-secondary mb-0">
+                            <a href="{{ route('shops.create') }}" class="fs-17 fw-700 bg-warning text-dark rounded-2 px-3 py-1 ">Apply Now</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <!-- Newsletter Section -->
+            <div class="col-lg-2 col-md-12 text-center text-md-start">
+                <h5 class="fs-20 fw-bold">Newsletter</h5>
+                <p class="fs-17">Sign up for our newsletter and get 10% off your first purchase</p>
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="Enter your e-mail">
+                    <button class="btn btn-secondary" type="button"><i class="fa-solid fa-arrow-right"></i></button>
+                </div>
+                <div class="d-flex justify-content-center justify-content-md-start">
+                    <a href="#" class="me-3 text-dark"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="me-3 text-dark"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="me-3 text-dark"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="me-3 text-dark"><i class="fab fa-youtube"></i></a>
+                    <a href="#" class="text-dark"><i class="fab fa-pinterest"></i></a>
+                </div>
+            </div>
+        </div>
+    <div class="row mt-5">
+    <!-- Images Section -->
+    <div class="col-lg-6 order-1 order-md-2 text-center text-md-end">
+        <span>
+            <img src="https://s.alicdn.com/@img/imgextra/i4/O1CN01dsw9V61Lbh0D1f9JG_!!6000000001320 fw-bold-2-tps-205-112.png" width="40px" alt="Visa">
+            <img src="https://s.alicdn.com/@img/imgextra/i4/O1CN01sXbha020agNJcLC4l_!!6000000006866-2-tps-148-112.png" width="40px" alt="Master">
+            <img src="https://s.alicdn.com/@img/imgextra/i4/O1CN010I5eGr1aDcQ82EcRH_!!6000000003296-2-tps-169-112.png" width="40px" alt="UnionPay">
+            <img src="https://s.alicdn.com/@img/imgextra/i1/O1CN017IIzE71MpGLv2nxMd_!!6000000001483-2-tps-260-112.png" width="70px" alt="PayPal">
+        </span>
+    </div>
+
+    <!-- Text Section -->
+    <div class="col-lg-6 order-2 order-md-1 text-center text-md-start">
+        <p class="mb-0 fs-17">©2025 Bighouz. All Rights Reserved.</p>
+    </div>
+   </div>
+
+    </div>
+</footer>
