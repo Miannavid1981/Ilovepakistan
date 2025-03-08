227402395f7b5fbb2f2b0309a94db1534fbbc71f
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-03-08 13:53:30 +0500

    Update newest_products_section.blade.php

--- resources/views/frontend/megamart/partials/newest_products_section.blade.php
+++ resources/views/frontend/megamart/partials/newest_products_section.blade.php
@@ -39,14 +39,18 @@
                 @foreach ($newest_products as $key => $new_product)
                 <div class="col-6 col-md-4 col-lg product-custom-col position-relative has-transition hov-animate-outline">
                     @php
-                        $seller_map = \App\Models\ProductSellerMap::where('product_id', $new_product->id)->where('seller_id', $new_product->user_id)->where('source_seller_id',$new_product->user_id )->first();
+                        $seller_id =  $new_product->user_id ?? null;
+                        $seller_map = \App\Models\ProductSellerMap::where('product_id', $new_product->id)->where('seller_id',  $seller_id )->where('source_seller_id', $seller_id  )->first();
                         $encrypted_skin = $seller_map->encrypted_hash ?? '';
                         $product_url = url('/product/' . $new_product->slug . '/' . $encrypted_skin);
                         $new_product->product_custom_url = $product_url;
-                        dd($new_product);
+                        // dd($new_product);
                         
                     @endphp
-                    @include('frontend.'.get_setting('homepage_select').'.partials.product_box_1',['product' => $new_product])
+
+                    @if( $seller_id )
+                        @include('frontend.'.get_setting('homepage_select').'.partials.product_box_1',['product' => $new_product])
+                    @endif
                 </div>
                 @endforeach
             </div>
