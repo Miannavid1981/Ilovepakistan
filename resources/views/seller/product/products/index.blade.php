60ac16f797ba6ba945e42122d2a91b1276071c65
Zohaib Zafar <zohaibzafar463@gmail.com>
2025-03-22 03:24:52 +0500

    Update index.blade.php

--- resources/views/seller/product/products/index.blade.php
+++ resources/views/seller/product/products/index.blade.php
@@ -94,7 +94,9 @@
                             <th width="30%">{{ translate('Name')}}</th>
                             {{-- <th data-breakpoints="md">{{ translate('Category')}}</th> --}}
                             <th data-breakpoints="md">{{ translate('Current Qty')}}</th>
-                            <th>{{ translate('Base Price')}}</th>
+                            <th>{{ translate('Listing Price')}}</th>
+                            <th>{{ translate('Platform Fee')}}</th>
+                            <th>{{ translate('Your sale')}}</th>
                             @if(get_setting('product_approve_by_admin') == 1)
                                 <th data-breakpoints="md">{{ translate('Approval')}}</th>
                             @endif
@@ -107,8 +109,99 @@
                     <tbody>
                         @foreach ($products as $key => $product)
                                 @php
+
+            
+                                    
+                                    $item_price = discount_in_percentage($product) > 0 ? home_discounted_base_price($product, false) : home_base_price($product, false);
+                                
+                                    $admin_user = \App\Models\User::where('email', 'admin@bighouz.com')->first();
+                                    $admin_id = $admin_user->id;
+
+                                    $target_cat = \App\Models\Category::where("id",$product->category_id)->first();
+                                        
+
+                                    $admin_commission_type = null;
+                                    $seller_commission_type = null;
+                                    
+                                    $admin_commission_rate = null;
+                                    $seller_commission_rate = null;
+                                    $brand_profit_amount = null;
+                                    $seller_profit = null;
+                                    $admin_profit_per_amount = null;
+                                    $admin_profit_final_amount = null;
+                                    $commission = 0;
+
+
+                                    if($target_cat->commission == 1){
+                                        $commission = 1;
+                                        $admin_commission_type = $target_cat->commission_rate_type;
+                                        $seller_commission_type = $target_cat->seller_commission_rate_type;
+
+                                        $admin_commission_rate = (float) $target_cat->commision_rate;
+                                        $seller_commission_rate = (float)  $target_cat->seller_commission_rate;
+
+                                    } else {
+
+                                        if($product->commission == 1){
+                                            $commission = 1;
+                                            $admin_commission_type = $product->admin_commission_type;
+                                            $seller_commission_type = $product->seller_commission_type;
+                                            
+                                            $admin_commission_rate = (float)  $product->admin_commission_rate;
+                                            $seller_commission_rate = (float)  $product->seller_commission_rate;
+                                        }
+                                        
+
+                                    }
+                                    
+                                    
+                            
+
+                                    if($commission ) {
+                                        
+                                        if( !empty($admin_commission_type) && !empty($admin_commission_rate)  ){
+
+                                            // Calculate admin profit per amount depending on the commission type (percentage or fixed amount)
+                                            if ($admin_commission_type === 'percentage') {
+                                                $admin_profit_per_amount = get_percentage_amount($admin_commission_rate, $item_price);
+                                            } else {
+                                                // If it's an amount, use it directly
+                                                $admin_profit_per_amount = $admin_commission_rate;
+                                            }
+                                            
+                                            $brand_profit_amount = $item_price - $admin_profit_per_amount;
+                                    
+                                            $seller_profit = 0;
+                                            
+
+                                                if( !empty($seller_commission_type) && !empty($seller_commission_rate)  ){
+                                                    // Handle seller commission
+                                                    if ($seller_commission_type === 'percentage') {
+                                                        $seller_profit_per_amount = get_percentage_amount($seller_commission_rate, $admin_profit_per_amount);
+                                                        $seller_profit = (int) ($seller_commission_rate / 100) * $admin_profit_per_amount;
+                                                    } else {
+                                                        // If it's a fixed amount, use the fixed value
+                                                        $seller_profit_per_amount = $seller_commission_rate;
+                                                        $seller_profit =  (int)  $seller_commission_rate; // Fixed amount, so no percentage calculation needed
+                                                    }
+                                                }
+                                            
+                                            
+                                            // Final admin profit after subtracting seller's profit
+                                            $admin_profit_final_amount = $admin_profit_per_amount - $seller_profit;
+                                            
+                                            // Assign final admin profit to order detail
+                                        
+                                            
+                                            
+                                            
+
+                                        } 
+                                    }
+
+
                                     $seller_id =  $product->user_id ?? null;
-                                    $seller_map = \App\Models\ProductSellerMap::where('source_seller_id', $seller_id  )->first();
+                                    $seller_map = \App\Models\ProductSellerMap::where('source_seller_id', $seller_id  )->where('product_id', $product->id)->first();
                                     $encrypted_skin = $seller_map->encrypted_hash ?? '';
                                     $product_url = url('/product/' . $product->slug . '/' . $encrypted_skin);
                                 @endphp
@@ -142,7 +235,9 @@
                                         echo $qty;
                                     @endphp
                                 </td>
-                                <td>{{ $product->unit_price }}</td>
+                                <td>{{ single_price($product->unit_price) }}</td>
+                                <td>@if($admin_profit_per_amount > 0)<p class="text-secondary">-{{ single_price($admin_profit_per_amount) }} <p>@endif</td>
+                                <td>@if($brand_profit_amount > 0)<p class="text-success">{{ single_price($brand_profit_amount) }} </p>@endif</td>
                                 @if(get_setting('product_approve_by_admin') == 1)
                                     <td>
                                         @if ($product->approved == 1)
