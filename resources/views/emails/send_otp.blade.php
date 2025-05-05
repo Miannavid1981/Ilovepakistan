b013042e58f4d5d7431fadb7b2c965e079f3ceda
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-05 10:58:54 +0500

    Update send_otp.blade.php

--- resources/views/emails/send_otp.blade.php
+++ resources/views/emails/send_otp.blade.php
@@ -1,4 +1,8 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Seller Registration OTP</title>
   <style>
     body {
       font-family: sans-serif;
@@ -27,7 +31,8 @@
       color: #888888;
     }
   </style>
-
+</head>
+<body>
   <div class="email-wrapper">
     
 
@@ -45,4 +50,5 @@
       <a href="https://www.bighouz.com" style="color:#1a73e8;">www.bighouz.com</a>
     </p>
   </div>
-
+</body>
+</html>
