c8ec85ff9eba8b63dc2da66855d8af02d3a84804
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-18 05:29:50 +0500

    Update invoice.blade.php

--- resources/views/backend/invoices/invoice.blade.php
+++ resources/views/backend/invoices/invoice.blade.php
@@ -197,7 +197,7 @@
                     <strong  style="text-align: end">Invoice #:</strong>  {{ $main_order_id }} </span><br>
                     <strong>Print Date:</strong> {{ \Carbon\Carbon::now()->format('F j, Y \a\t h:i A') }}
                 </td>
-                <td style="width: 90px">
+                <td style="width: 90px' vertical-align: middle">
                    <div style="aspec-ratio: 1 / 1">
                     @php
                     $qrCode = base64_encode(QrCode::format('png')->size(100)->generate($order->id));
