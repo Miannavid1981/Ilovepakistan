ab05ff5b8699a40b5d9966271cd7b61ad2d039cf
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-22 13:01:10 +0500

    Update index.blade.php

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -530,7 +530,7 @@
                     </div>
                     <div class="col-12">
                         
-                        <input class="form-control " id="phone" type="number" maxlength="11" placeholder="03XXXXXXXXX" name="phone" value="{{  auth()->user()->phone }}" autocomplete="off" required>
+                        <input class="form-control " id="phone" type="text" maxlength="11" placeholder="03XXXXXXXXX" name="phone" value="{{  auth()->user()->phone }}" autocomplete="off" required>
                     </div>
                 </div>
                 <h5 class=" mt-4">Shipping Information</h5>
