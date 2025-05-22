00ced70ba34964d513853983639e2fe2493bc373
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-22 09:55:13 +0500

    Update Address.php

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -9,7 +9,7 @@ class Address extends Model
 {
     use PreventDemoModeChanges;
 
-    protected $fillable = ['set_default'];
+    protected $fillable = ['set_default', 'area', 'landmark', 'state_id', 'city_id', 'country_id', 'address_label', 'address_type', 'longitude', 'latitude', 'phone', 'postal_code', 'address'];
 
     public function user()
     {
