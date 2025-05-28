42e198f84b61e38dcdca32d0b6d9ec52127fcc6c
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-28 12:29:27 +0500

    Update payment_methods.blade.php

--- resources/views/frontend/checkout/inc/payment_methods.blade.php
+++ resources/views/frontend/checkout/inc/payment_methods.blade.php
@@ -363,14 +363,14 @@
             </button>
           
         </div>
-        {{ $total <= $user_wallet_balance ? 
-            `<div id="bighouz_wallet" class="collapse" aria-labelledby="mobile_walletHeading" data-parent="#paymentAccordion">
+        @if ($total <= $user_wallet_balance) 
+            <div id="bighouz_wallet" class="collapse" aria-labelledby="mobile_walletHeading" data-parent="#paymentAccordion">
             <div class="card-body payment-method-details p-4 bg-light">
                 You will be redirected to PayPal to complete your purchase securely.
             </div>
-            </div>`
+            </div>
             : '' 
-        }}
+        @endif
       </div>
     <div class="card mb-0 shadow-none p-0" style="border: 1px solid #c1c1c1;">
         <div class="card-header p-0" id="cash_on_counterHeading">
