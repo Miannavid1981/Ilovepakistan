c92a1b97ac04872ed8774b322d052e4fca49f38b
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-28 12:08:14 +0500

    Update payment_methods.blade.php

--- resources/views/frontend/checkout/inc/payment_methods.blade.php
+++ resources/views/frontend/checkout/inc/payment_methods.blade.php
@@ -344,12 +344,15 @@
           
             <button class="btn btn-link text-decoration-none w-100 p-0 collapsed" type="button" data-toggle="collapse" data-target="#bighouz_wallet">
               <label class="payment-method-header d-flex justify-content-between w-100 mb-0 p-2 px-3">
-                  <div class="  text-dark d-flex flex-column align-items-center mb-0">
-                    <input type="radio" class=" mb-0 me-2" name="payment_method" value="bighouz_wallet" {{ $total >  $user_wallet_balance ? 'disabled' : '' }}> Bighouz Wallet
-                    @if ($total > $user_wallet_balance) 
-                    <br>
-                        <small class="text-danger"> Insufficient Funds </small>
-                    @endif
+                  <div class="  text-dark d-flex align-items-center mb-0">
+                    <input type="radio" class=" mb-0 me-2" name="payment_method" value="bighouz_wallet" {{ $total >  $user_wallet_balance ? 'disabled' : '' }}> 
+                        <div class="d-flex flex-column">
+                            Bighouz Wallet
+                            @if ($total > $user_wallet_balance) 
+                            <br>
+                                <small class="text-danger"> Insufficient Funds </small>
+                            @endif
+                        </div>
                   </div>
                   <span>
                     <img src="{{ uploaded_asset(get_setting('site_icon')) }}" style="width: 35px; height: auto" alt="Discover">
