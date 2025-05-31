888a7911497632add80cf8c97b6701643b22d432
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-05-31 06:33:16 +0500

    Update index.blade.php

--- resources/views/backend/deposit_requests/index.blade.php
+++ resources/views/backend/deposit_requests/index.blade.php
@@ -55,12 +55,12 @@
                                             <td>
                                                 <div class="d-flex ">
                                                     <a href="{{ route('admin_business_directory.edit', $item->id) }}" class="btn btn-success btn-sm me-2">
-                                                        <i class="la la-edit fs-15"></i>
+                                                        <i class="la la-check fs-15"></i>
                                                     </a>
                                                     <form action="{{ route('admin_business_directory.destroy', ['business_directory' => $item->id]) }}" method="POST" style="display:inline;">
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this?');"><i class="la la-trash fs-15"></i></button>
+                                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this?');"><i class="la la-close fs-15"></i></button>
                                                     </form>
                                                     
                                                 </div>
