8e95608145c3bae1dd201ddddd2bf001d3848333
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-06-02 00:56:09 +0500

    Update WalletController.php

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -128,7 +128,7 @@ class WalletController extends Controller
 
         // Add to wallet
         $credit_status = \App\Models\Wallet::credit([
-            'wallet_id' => auth()->user()->wallet->id,
+            'wallet_id' => 0,
             'user_id' => $request->user_id,
             'amount' => $request->amount,
             'type' => 'credit',
