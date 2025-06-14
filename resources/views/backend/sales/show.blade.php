a7e28913000bcc100db5f5c7ca1b4b1926f2217b
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-06-14 16:55:49 +0500

    Update show.blade.php

--- resources/views/backend/sales/show.blade.php
+++ resources/views/backend/sales/show.blade.php
@@ -312,17 +312,12 @@
                                                         <div class="popover-content">
                                                            
 
-                                                            @if($orderDetail->source_seller_id != $orderDetail->seller_id) 
-                                                                @php
-                                                                    $import_seller = \App\Models\User::where('id', $orderDetail->seller_id)->first();
-                                                                @endphp
-                                                                <div class="d-flex gap-2 align-items-center ">
-                                                                    <div class="w-40px h-40px ">
-                                                                        <img src="{{ uploaded_asset($import_seller->avatar_original) }}" class="w-100 h-100 object-cover rounded-3 ">
-                                                                    </div>
-                                                                    <p class="mb-0 ">{{ $import_seller->name }}</p>
+                                                            <div class="d-flex gap-2 align-items-center ">
+                                                                <div class="w-40px h-40px ">
+                                                                    <img src="{{ uploaded_asset($brand_sold_seller->avatar_original) }}" class="w-100 h-100 object-cover rounded-3 ">
                                                                 </div>
-                                                            @endif
+                                                                <p class="mb-0 ">{{ $brand_sold_seller->name }}</p>
+                                                            </div>
                                                             <strong>Type:</strong> Brand Partner<br>
                                                             <strong><i class="fa fa-envelope"></i>&nbsp;</strong> john@example.com<br>
                                                             <strong>Shop:</strong> Doe Mart
