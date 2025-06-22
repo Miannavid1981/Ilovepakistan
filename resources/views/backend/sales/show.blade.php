fd52db23598f736c0223f6bdb6ccfc6f9b4b1547
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-06-22 16:09:30 +0500

    Update show.blade.php

--- resources/views/backend/sales/show.blade.php
+++ resources/views/backend/sales/show.blade.php
@@ -467,7 +467,7 @@
                     <h4>Payment Receipts</h4>
                     <div style="display: flex; flex-wrap: wrap">
                     @foreach($receipts as $receipt)
-                        <img src="{{ static_asset('storage/' . $receipt) }}" >
+                        <img src="{{ static_asset('storage/' . $receipt) }}" style="width: 300px;">
                         
                     @endforeach
                     </div>
