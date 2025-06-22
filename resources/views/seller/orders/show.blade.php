c6929007f93315cf0654e9964a7fb1428bf46b6c
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-06-22 13:02:39 +0500

    Update show.blade.php

--- resources/views/seller/orders/show.blade.php
+++ resources/views/seller/orders/show.blade.php
@@ -283,7 +283,7 @@
                                         
                                     <td class="text-center">{{ $orderDetail->quantity }}</td>
                                     
-                                    <td class="text-center">{{ single_price($row_sale * $orderDetail->quantity) }}</td>
+                                    <td class="text-center">{{ single_price($row_sale ) }}</td>
                                 </tr>
                                 @php
                                 $overall_sale += $row_sale; 
