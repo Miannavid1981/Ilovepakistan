80fcc12bef423c69397afe8cd41b33f79a20beb3
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-07-02 19:10:07 +0500

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -329,7 +329,7 @@ class OrderController extends Controller
                                 // If it's an amount, use it directly
                                 $admin_profit_per_amount = $admin_commission_rate;
                             }
-                            $admin_profit_per_amount = $cartItem['quantity'] * $admin_profit_per_amount;
+                            $admin_profit_per_amount =  $admin_profit_per_amount;
                             $brand_profit_amount = $item_price - $admin_profit_per_amount;
                           
                             // if( $source_seller_id != $seller_id   ){
