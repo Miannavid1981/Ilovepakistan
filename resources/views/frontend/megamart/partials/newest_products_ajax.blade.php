614d5227c7317a946374fb9e7b89cc781ab574df
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-08-01 21:25:18 +0500

    Update newest_products_ajax.blade.php

--- resources/views/frontend/megamart/partials/newest_products_ajax.blade.php
+++ resources/views/frontend/megamart/partials/newest_products_ajax.blade.php
@@ -1,3 +1,17 @@
-@foreach($newest_products as $product)
-    @include('frontend.megamart.partials.product_box_1', ['product' => $product])
-@endforeach
+ @foreach ($newest_products as $key => $new_product)
+    <div class="col-6 col-md-4 col-lg product-custom-col position-relative has-transition hov-animate-outline">
+        @php
+            $seller_id =  $new_product->user_id ?? null;
+            $seller_map = \App\Models\ProductSellerMap::where('product_id', $new_product->id)->where('seller_id',  $seller_id )->where('source_seller_id', $seller_id  )->first();
+            $encrypted_skin = $seller_map->encrypted_hash ?? '';
+            $product_url = url('/product/' . $new_product->slug . '/' . $encrypted_skin);
+            $new_product->product_custom_url = $product_url;
+            // dd($new_product);
+            
+        @endphp
+
+        @if( $seller_id )
+            @include('frontend.'.get_setting('homepage_select').'.partials.product_box_1',['product' => $new_product])
+        @endif
+    </div>
+    @endforeach
