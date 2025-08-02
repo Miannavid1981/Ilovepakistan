aff2c584a33bb7468299b9d8543fcef777e76fa0
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-08-02 12:56:21 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -63,7 +63,7 @@ class HomeController extends Controller
         $page = $request->input('page', 1);
         $perPage = 12;
 
-        $query = Product::isApprovedPublished()->latest();
+        $query = Product::latest();
 
         $products = filter_products($query)->paginate($perPage, ['*'], 'page', $page);
         $nextpage_products = filter_products($query)->paginate($perPage, ['*'], 'page', $page+1 );
