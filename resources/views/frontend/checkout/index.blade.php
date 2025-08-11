4b2efc0a38d1305f361ccbc0a4f8842626b8f049
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-08-11 20:43:38 +0500

    Update index.blade.php

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -634,7 +634,7 @@
                         @endif
                         @if($delivery_type_settings->family_friends)
                             <div class="col-12 col-md-4 mb-2">
-                                <div class="w-100 delivery_type fs-16 modern-button button-family">
+                                <div class="w-100 delivery_type fs-16 modern-button button-others">
                                     <input type="radio" class="form-check " name="delivery_type" value="family_friends" >
                                        <svg viewBox="0 0 24 24"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zM8 11c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 3zm8 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zM8 13c-.29 0-.62.02-.97.05C6.39 13.3 5 14.02 5 15.5V19h5v-2.5c0-.77.3-1.47.78-2.05-.89-.28-1.88-.45-2.78-.45z"/></svg>
                                         Family & Friends
