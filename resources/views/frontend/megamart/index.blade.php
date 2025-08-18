98837d68368575a3ea58a1e71eaeaab34759ef0d
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-08-18 13:42:07 +0500

    Update index.blade.php

--- resources/views/frontend/megamart/index.blade.php
+++ resources/views/frontend/megamart/index.blade.php
@@ -358,10 +358,10 @@
             </div>
             <div class="left-section d-flex flex-column h-100" style="background-image: url(https://litech.com.pk/wp-content/uploads/2024/08/tigfox-banner.jpg); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px">
             </div>
-            <div class="left-section d-flex flex-column h-100" style="background-image: url({{ static_asset('assets/img/headphone-earphones-electronics-gadgets-flyer-design-template-f2a41ff7a4f5e05ae8bb552d8903f906_screen.webp') }}); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px">
+            {{-- <div class="left-section d-flex flex-column h-100" style="background-image: url({{ static_asset('assets/img/headphone-earphones-electronics-gadgets-flyer-design-template-f2a41ff7a4f5e05ae8bb552d8903f906_screen.webp') }}); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px">
             </div>
             <div class="left-section d-flex flex-column h-100" style="background-image: url({{ static_asset('assets/img/1_8Fx_5pUpS4wda42aGnuuuQ.webp') }}); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px;">
-            </div>
+            </div> --}}
         </div>
 
                 <!-- Left Section -->
