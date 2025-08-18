79fd8c781f102e7676458ab0d62c59ada3aee6fb
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-08-18 13:37:54 +0500

    Update index.blade.php

--- resources/views/frontend/megamart/index.blade.php
+++ resources/views/frontend/megamart/index.blade.php
@@ -416,13 +416,10 @@
     word-break: break-word;
     text-align: center;
     max-width: 100px;
-    
-   
-   
-    font-family: 'Public Sans' !important;
+    font-family: 'Aeonik-Semibold' !important;
     font-weight: 600;
     font-size: 13px !important;
-    letter-spacing: -0.5px !important;">   {{$category->name}}</p>
+    letter-spacing: 0px !important;">   {{$category->name}}</p>
                         </a>
                     
                 @endforeach
