3dc8a844e119e87e6c1c14d40ddabaabed7a10b6
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-08-20 18:17:41 +0500

    Update index.blade.php

--- resources/views/frontend/megamart/index.blade.php
+++ resources/views/frontend/megamart/index.blade.php
@@ -357,7 +357,7 @@
     <div class="bighouz-business">
 
         <div class="main_banner_slider" >
-            <div class="left-section d-flex flex-column h-100" style="background-image: url(https://static.vecteezy.com/system/resources/thumbnails/008/520/419/small_2x/go-solar-illustration-for-infographic-solar-panel-sun-clean-electricity-green-renewable-sustainable-energy-self-awareness-innovative-energetic-environment-nature-banner-graphic-template-vector.jpghttps://maxpower.com.pk/wp-content/uploads/2025/05/vfd-mea-banner-1-1-scaled.jpg); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px">
+            <div class="left-section d-flex flex-column h-100" style="background-image: url(https://static.vecteezy.com/system/resources/thumbnails/008/520/419/small_2x/go-solar-illustration-for-infographic-solar-panel-sun-clean-electricity-green-renewable-sustainable-energy-self-awareness-innovative-energetic-environment-nature-banner-graphic-template-vector.jpg); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px">
             </div>
             <div class="left-section d-flex flex-column h-100" style="background-image: url(https://litech.com.pk/wp-content/uploads/2024/08/tigfox-banner.jpg); background-size: cover; background-position: center; background-color: #d3e7ff ; border-radius: 20px">
             </div>
