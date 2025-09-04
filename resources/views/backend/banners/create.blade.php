afbeda89b03ceda868bf83f8e54248ecc5da414a
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-09-04 07:27:16 +0500

    Update create.blade.php

--- resources/views/backend/banners/create.blade.php
+++ resources/views/backend/banners/create.blade.php
@@ -4,7 +4,18 @@
     <h2>Add Banner</h2>
     <form action="{{ route('admin.home_banners.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="file" name="image" required>
-        <button class="btn btn-success">Save</button>
+        <div class="form-group">
+            <label for="image">Banner Image</label>
+            
+            {{-- AIZ uploader or your custom uploader --}}
+            <div class="input-group" data-toggle="aizuploader" data-type="image">
+                <div class="input-group-prepend">
+                    <div class="input-group-text bg-soft-secondary font-weight-medium">Browse</div>
+                </div>
+                <div class="form-control file-amount">Choose Image</div>
+                <input type="hidden" name="image" class="selected-files">
+            </div>
+            <div class="file-preview box sm"></div>
+        </div>
     </form>
 @endsection
