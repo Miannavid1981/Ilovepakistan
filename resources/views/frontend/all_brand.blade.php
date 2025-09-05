cd7a8c8712b39c05e312c560f2187060592116f0
php-artizan <105066051+php-artizan@users.noreply.github.com>
2025-09-05 12:23:56 +0500

    Update all_brand.blade.php

--- resources/views/frontend/all_brand.blade.php
+++ resources/views/frontend/all_brand.blade.php
@@ -27,6 +27,12 @@
             <div class="bg-white px-3 pt-3">
                 <div class="row row-cols-xxl-6 row-cols-xl-6 row-cols-lg-4 row-cols-md-4 row-cols-3 gutters-16 border-top border-left">
                     @foreach ($brands as $brand)
+
+
+                        @php
+                            $products_count = \App\Models\Product::where('brand_id', $brand->id )->count();
+                        @endphp
+                        @if($products_count > 0 )
                         <div class="col text-center border-right border-bottom hov-scale-img has-transition hov-shadow-out z-1">
                             <a href="{{ route('products.brand', $brand->slug) }}" class="d-block p-sm-3">
                                 <img src="{{ uploaded_asset($brand->logo) }}" class="lazyload h-md-100px mx-auto has-transition p-2 p-sm-4 mw-100"
@@ -34,6 +40,7 @@
                                 <p class="text-center text-dark fs-14 fw-700 mt-2">{{ $brand->getTranslation('name') }}</p>
                             </a>
                         </div>
+                        @endif
                     @endforeach
                 </div>
             </div>
